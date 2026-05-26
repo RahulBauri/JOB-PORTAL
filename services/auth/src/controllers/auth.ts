07dc16bb93d28b59f84f68d27a81db78981d76c4
rahul bauri <kumarrahulbauri2000@gmail.com>
2026-05-26 19:30:07 +0530

    added code to upload file to claudinary

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import getBuffer from '../utils/buffer.js';
 import { sql } from '../utils/db.js';
 import ErrorHandler from '../utils/errorHandler.js';
 import { TryCath } from '../utils/TryCatch.js';
@@ -34,10 +36,34 @@ export const registerUser = TryCath(async (req, res, next) => {
   } else if (role === 'jobseeker') {
     const file = req.file;
 
+    if (!file) {
+      return next(
+        new ErrorHandler(400, 'Resume file is required for jobseekers'),
+      );
+    }
+
+    const fileBuffer = getBuffer(file);
+
+    if (!fileBuffer || !fileBuffer.content) {
+      return next(
+        new ErrorHandler(
+          500,
+          'Failed to generate buffer from the provided file',
+        ),
+      );
+    }
+
+    const { data } = await axios.post(
+      `${process.env.UPLOAD_SERVICE}/api/utils/upload`,
+      {
+        buffer: fileBuffer.content,
+      },
+    );
+
     const [user] = await sql`
-      INSERT INTO users (name,email,password,phone_number, role) 
-      VALUES (${name}, ${email}, ${hashPassword}, ${phoneNumber}, ${role}) 
-      RETURNING user_id, name, email, phone_number, role, created_at
+      INSERT INTO users (name,email,password,phone_number, role, bio, resume, resume_public_id) 
+      VALUES (${name}, ${email}, ${hashPassword}, ${phoneNumber}, ${role}, ${bio}, ${data.url}, ${data.public_id}) 
+      RETURNING user_id, name, email, phone_number, role, bio, resume, created_at
     `;
   }
 });
---
+++ services/utils/src/routes.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { v2 as cloudinary } from 'cloudinary';
+
+const router = express.Router();
+
+router.post('/upload', async (req, res) => {
+  try {
+    const { buffer, public_id } = req.body;
+
+    if (public_id) {
+      await cloudinary.uploader.destroy(public_id);
+    }
+
+    const cloud = await cloudinary.uploader.upload(buffer);
+
+    res.json({ url: cloud.secure_url, public_id: cloud.public_id });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
