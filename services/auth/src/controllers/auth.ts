7e895f7d42d2231ac41ec38dc3389dc81f419647
rahul bauri <kumarrahulbauri2000@gmail.com>
2026-06-03 08:42:23 +0530

    code for kafka producer and forgot password controller done

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -6,6 +6,8 @@ import { TryCath } from '../utils/TryCatch.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import e from 'express';
+import { forgotPasswordTemplate } from '../template.js';
+import { publishToTopic } from '../producer.js';
 
 export const registerUser = TryCath(async (req, res, next) => {
   const { name, email, password, phoneNumber, role, bio } = req.body;
@@ -120,4 +122,46 @@ export const loginUser = TryCath(async (req, res, next) => {
   );
 
   res.json({ message: 'User Logged in', userObject, token });
+});
+
+export const forgotPassword = TryCath(async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return next(new ErrorHandler(400, 'email is required'));
+  }
+
+  const users = await sql`
+    SELECT user_id, email 
+    FROM users
+    WHERE email = ${email}
+  `;
+
+  if (users.length === 0) {
+    return res.json({
+      message: 'If that email exists, we have sent a reset link',
+    });
+  }
+
+  const user = users[0];
+
+  const resetToken = jwt.sign(
+    { email: user.email, type: 'reset' },
+    process.env.JWT_SEC as string,
+    { expiresIn: '15m' },
+  );
+
+  const resetLink = `${process.env.FRONTEND_URL}/reset/${resetToken}`;
+
+  const message = {
+    to: email,
+    subject: 'RESET Your Password - HireHeaven',
+    html: forgotPasswordTemplate(resetLink),
+  };
+
+  await publishToTopic('send-mail', message).catch((error) => {
+    console.error('failed to send message', error);
+  });
+
+  res.json({ message: 'If that email exists, we have sent a reset link' });
 });
