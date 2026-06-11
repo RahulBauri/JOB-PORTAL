87fdaa51d0a9b54155ccef45831c2277b3c9b564
rahul bauri <kumarrahulbauri2000@gmail.com>
2026-06-12 03:26:42 +0530

    user service completed

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -127,3 +127,93 @@ export const updateResume = TryCath(
     res.json({ message: 'Resume updated', updatedUser });
   },
 );
+
+export const addSkillToUser = TryCath(
+  async (req: AuthenticatedRequest, res, next) => {
+    const userId = req.user?.user_id;
+
+    const { skillName } = req.body;
+
+    if (!skillName || skillName.trim() === '') {
+      return next(new ErrorHandler(400, 'Please provide a skill name to add'));
+    }
+
+    let wasSkillAdded = false;
+
+    try {
+      await sql`BEGIN`;
+
+      const users = await sql`
+        SELECT user_id FROM users
+        WHERE user_id = ${userId}
+      `;
+
+      if (users.length === 0) {
+        return next(new ErrorHandler(404, 'User not found'));
+      }
+
+      const [skill] = await sql`
+        INSERT INTO skills (name) 
+        VALUES (${skillName.trim()})
+        ON CONFLICT (name)  
+        DO UPDATE SET name = EXCLUDED.name
+        RETURNING skill_id
+      `;
+
+      const skillId = skill.skill_id;
+
+      const insertionResult = await sql`
+        INSERT INTO user_skills (user_id, skill_id)
+        VALUES (${userId}, ${skillId})
+        ON CONFLICT (user_id, skill_id)  
+        DO NOTHING 
+        RETURNING user_id
+      `;
+
+      if (insertionResult.length > 0) {
+        wasSkillAdded = true;
+      }
+
+      await sql`COMMIT`;
+    } catch (error) {
+      await sql`ROLLBACK`;
+      throw error;
+    }
+
+    if (!wasSkillAdded) {
+      return res
+        .status(200)
+        .json({ message: 'User already possesses this skill' });
+    }
+
+    res.json({ message: `Skill ${skillName.trim()} is added successfully` });
+  },
+);
+
+export const deleteSkillFromUser = TryCath(
+  async (req: AuthenticatedRequest, res, next) => {
+    const user = req.user;
+
+    const { skillName } = req.body;
+
+    if (!skillName || skillName.trim() === '') {
+      return next(
+        new ErrorHandler(400, 'Please provide a skill name to delete'),
+      );
+    }
+
+    const result = await sql`
+    DELETE FROM user_skills 
+    WHERE user_id = ${user?.user_id} AND skill_id = (SELECT skill_id FROM skills WHERE name = ${skillName.trim()})
+    RETURNING user_id
+  `;
+
+    if (result.length === 0) {
+      return next(
+        new ErrorHandler(404, `Skill ${skillName.trim()} was not found`),
+      );
+    }
+
+    res.json({ message: `Skill ${skillName.trim()} was deleted successfully` });
+  },
+);
